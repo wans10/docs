3128a3ad97f276cd0c03c6aa07ddb73e5b125100
wans10 <51012039+wans10@users.noreply.github.com>
2025-09-11 09:34:15 +0800

    Delete lint-staged.config.js

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  "**/*.ts{,x}": ["eslint --fix --max-warnings 0", "pnpm format"],
-  "**/*.{js,json,yml,html,css,less,scss,md}": "pnpm format",
-  "**/package.json": () => "pnpm install --immutable",
-};
